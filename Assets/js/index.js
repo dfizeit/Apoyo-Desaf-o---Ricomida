85319786100b08bac8df9a1b445dcb3ea0427078
dfizit <german.tabillo.m@mail.pucv.cl>
2023-04-18 22:01:50 -0400

    cambios js (fallido) / index para acomodar texto

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
